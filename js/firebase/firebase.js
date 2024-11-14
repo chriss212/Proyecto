7ee78d1e23f40cb4e97bb6d0ee6290c98fca3196
diegocabezas004 <diegocabezas72@gmail.com>
2024-11-14 12:46:11 -0600

    registro oki doki login iniciado

--- js/firebase/firebase.js
+++ js/firebase/firebase.js
@@ -1,39 +1,58 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getAuth,createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-import { getFirestore, doc, addDoc, setDoc, getDoc, getDocs, collection, query, where } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { getAuth,createUserWithEmailAndPassword,signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { getFirestore, doc, addDoc, setDoc, getDoc, getDocs, collection, query, where} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
   // Your web app's Firebase configuration
   const firebaseConfig = {
-    apiKey: "",
-    authDomain: "pagina-noticias-80c7e.firebaseapp.com",
-    databaseURL: "https://pagina-noticias-80c7e-default-rtdb.firebaseio.com",
-    projectId: "pagina-noticias-80c7e",
-    storageBucket: "pagina-noticias-80c7e.firebasestorage.app",
-    messagingSenderId: "846300969774",
-    appId: "1:846300969774:web:846e2a46ed0abb8eadd8bf",
-    measurementId: "G-9K7DLK6RRP"
+    apiKey: "AIzaSyCB8NYhVXAzQUlinqCH_WOfRazSHDDbVR4",
+    authDomain: "pagina-noticias-45a5c.firebaseapp.com",
+    projectId: "pagina-noticias-45a5c",
+    storageBucket: "pagina-noticias-45a5c.firebasestorage.app",
+    messagingSenderId: "808486829176",
+    measurementId: "G-PBN9GEDN78",
+    appId: "1:808486829176:web:03744a4ef3cd13f95b418d"
   };
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   export const auth= getAuth(app);
   const db=getFirestore(app);
+  
 
-    // Función para registrar usuarios
-  export async function registerUser(email, password, fullname, username){
-  try{
-      const userCredencial=await createUserWithEmailAndPassword(auth,email,password);
-      const user=userCredencial.user;
-      await setDoc(doc(db,"users",user.uid),{
-        fullname,
-        email,
-        username,
-        role:"user"
-      })
-      console.log('Usuario registrado exitosamente: ', userCredencial.user);
-      return true;
+
+  export async function registerUser(email,password,username,fullName){
+    try{
+        const userCredencial=await createUserWithEmailAndPassword(auth,email,password);
+        const user=userCredencial.user;
+        await setDoc(doc(db,"users",user.uid),{
+          username,
+          fullName,
+          email,
+          role:"user"
+        })
+        console.log('Usuario registrado exitosamente: ', userCredencial.user);
+        return true;
+      }
+    catch(error){
+        console.log('Error:' , error.message);
+        return false;
     }
-  catch(error){
-      console.log('Error:' , error.message);
-      return false;
-  }
+}
+
+  export async function loginUser(email, password){
+    try{
+      const userCredential = await signInWithEmailAndPassword(auth, email, password)
+      const user = userCredential.user;
+
+      const dataUser = await getDoc(doc(db, 'users', user.uid));
+
+      if (dataUser.exists()){
+        console.log("Inicio de sesión exitoso:", dataUser.data().role);
+        return dataUser.data();
+      } else {
+        throw new Error("El ususario no existe en la base de datos.");
+      }
+    } catch (error){
+      console.error("Error en el inicio de sesión:", error.message);
+      return null;
+    }
 }
--- js/login.js
+++ js/login.js
@@ -1,23 +1,46 @@
-import { registerUser } from "./firebase/firebase.js";
+import { registerUser, loginUser } from "./firebase/firebase.js";
 
 document.getElementById("registerForm").addEventListener("submit", async (e) => {
     e.preventDefault();
 
-    let fullname = document.getElementById("fullname").value;
-    let email = document.getElementById("r_email").value;
+    let correo = document.getElementById("registerEmail").value;
+    let password = document.getElementById("registerPassword").value;
     let username = document.getElementById("username").value;
-    let password = document.getElementById("r_password").value;
+    let fullName = document.getElementById("fullName").value;
 
-    const status = await registerUser(fullname, email, username, password)
+    const status = await registerUser(correo, password, username, fullName)
 
     if(status){
         alert("Usuario creado exitosamente");
 
-        document.getElementById("fullname").value = '';
-        document.getElementById("r_email").value = '';
+        document.getElementById("registerEmail").value = '';
+        document.getElementById("registerPassword").value = '';
         document.getElementById("username").value = '';
-        document.getElementById("r_password").value = '';
+        document.getElementById("fullName").value = '';
     }else{
         alert("Ya existe un usuario asociado a este correo");
+    }
+});
+
+// Inicio de sesión
+document.getElementById('loginForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    
+    const email = document.getElementById('loginEmail').value;
+    const password = document.getElementById('loginPassword').value;
+    
+    try {
+
+      const userData = await loginUser(email, password);
+      
+      if (userData) {
+
+      window.location.href = "home.html";
+      } else {
+        alert("Usuario y/o contraseña incorrectos.");
+      }
+    } catch (error) {
+      alert("Hubo un problema al iniciar sesión.");
+      console.error("Error al iniciar sesión:", error.message);
     }
 });
